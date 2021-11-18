6b942a6f51ec27a680ceeaedac5e9bae2eb0c326
jbaafi <jbaafi@aims.edu.gh>
2021-11-18 16:04:01 -0330

    Update data-science.R

--- data-science/data-science.R
+++ data-science/data-science.R
@@ -2,7 +2,29 @@
 #Date: 17-12-2021
 #Author: Joseph Baafi
 
+# Clear workspace
+rm(list = ls())
+
 # Load packages 
 library(tidyverse)
 library(dslabs)
 data(murders)
+library(ggplot2)
+
+# Plot a categorical variable against a contonous variable. This was a lab8 exercise that 
+# Michelle gave to me to figure out. 
+
+# Dataframe
+df <- data.frame(dimension=rep(c('1mm', '2mm'), 3), 
+                  trees=c('Alder', 'Alder', 'Birch', 'Birch', 'CP', 'CP'), 
+                  record=c(5.15, 6.78, 4.82, 5.85, 3.73, 5.18))
+
+# Plot 
+ggplot(data=df, aes(x=trees, y=record, group=dimension, color=dimension))+ 
+  geom_line()+
+  xlab("Tree species")+
+  ylab("Decomposition Rate")+
+  ylim(0, 8)+ 
+  theme_light()
+
+
