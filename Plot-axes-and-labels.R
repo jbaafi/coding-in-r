2228672fea1391ecc186e4a155503b4ec471cbd4
jbaafi <jbaafi@aims.edu.gh>
2022-10-04 11:01:51 -0230

    Update Plot-axes-and-labels.R

--- Plot-axes-and-labels.R
+++ Plot-axes-and-labels.R
@@ -26,3 +26,22 @@ mtext(text = "x-axis", side = 1, line = 3)
 plot(1:10, mgp = c(3, 1, 0))
 
 plot(1:10, mgp = c(3, 2, 0))
+
+
+# To increase font size of text in the title, labels and other places of a plot? 
+x <- rnorm(100)
+
+hist(x, xlim=range(x),
+     xlab= "Variable Lable", ylab="density", main="Title of plot", prob=TRUE)
+
+hist(x, xlim=range(x),
+     xlab= "Variable Lable", ylab="density", main="Title of plot", prob=TRUE, 
+     cex=1.5)
+
+hist(x, xlim=range(x),
+xlab= "Variable Lable", ylab="density", main="Title of plot", prob=TRUE, 
+cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+
+
+
+
