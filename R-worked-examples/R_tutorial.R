b3c92754cb39427876c9eadc4d2ecee7b2f76ae5
jbaafi <jbaafi@aims.edu.gh>
2022-11-24 13:38:11 -0330

    Update R_tutorial.R

--- R-worked-examples/R_tutorial.R
+++ R-worked-examples/R_tutorial.R
@@ -298,3 +298,27 @@ exp_den <- fit_exp$estimate*exp(-fit_exp$estimate*hist.data$mids)
 # This site is a great site for fitting data to a mathematical model/equation/function
 # https://martinlab.chem.umass.edu/r-fitting-data/
 
+# ------ This section deals with having two y-axis labels on the same plot
+#library
+library(latticeExtra)
+
+# create data
+set.seed(1)
+x <- 1:100
+var1 <- cumsum(rnorm(100))
+var2 <- var1^2
+data <- data.frame(x,var1,var2)
+
+
+# usual line chart
+xyplot(var1 + var2 ~ x, data, type = "l", col=c("steelblue", "#69b3a2") , lwd=2)
+
+# --> construct separate plots for each series
+obj1 <- xyplot(var1 ~ x, data, type = "l" , lwd=2, col="steelblue")
+obj2 <- xyplot(var2 ~ x, data, type = "l", lwd=2, col="#69b3a2")
+
+# --> Make the plot with second y axis:
+doubleYScale(obj1, obj2, add.ylab2 = TRUE, use.style=FALSE )
+
+# --> Make the plot with second y axis AND legend:
+doubleYScale(obj1, obj2, text = c("obj1", "obj2") , add.ylab2 = TRUE)
