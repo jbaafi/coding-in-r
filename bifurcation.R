b3a598ffeec45c8d803130f45767903bc8bf45f3
jbaafi <jbaafi@aims.edu.gh>
2023-02-13 09:23:46 -0330

    Update bifurcation.R

--- bifurcation.R
+++ bifurcation.R
@@ -185,4 +185,31 @@ if(interactive()){
   bifurcation(model, state, parms)
 }
 
+# -----------------------------------------------------------
+# The system will still run without the interactive function
+
+# The initial state of the system has to be
+# specified as a named vector of state values.
+state <- c(R = 0.05, C = 0.1)
+
+# Parameters has to be specified as a named vector of parameters.
+parms <- c(r = 0.5, K = 0.1, a = 5.0, h = 3.0, eps = 0.5, mu = 0.05)
+
+# The model has to be specified as a function that returns
+# the derivatives as a list. You can adapt the body below
+# to represent your model
+rosenzweig <- function(t, state, parms) {
+  with(as.list(c(state,parms)), {
+    dR = r*R*(1 - R/K) - a*R*C/(1 + a*h*R)
+    dC = eps*a*R*C/(1 + a*h*R) - mu*C
+    # The order of the derivatives in the returned list has to be
+    # identical to the order of the state variables contained in
+    # the argument `state`
+    return(list(c(dR, dC)))
+  })
+}
+
+bifurcation(rosenzweig, state, parms)
+
+
 
